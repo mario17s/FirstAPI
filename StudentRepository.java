2cde8ce0a7ed13e5cd8cdb87b64d8dc44bb91e9a
mario17s <monu.mario@yahoo.com>
2023-09-26 10:51:12 +0300

    ok

--- StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.api.repo;
-
-import com.example.api.student.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
---
+++ src/main/java/com/example/api/student/Student.java
@@ -0,0 +1,94 @@
+package com.example.api.student;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstName")
+    private String firstName;
+    @Column(name = "lastName")
+    private String lastName;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "dob")
+    private LocalDate dob;
+    @Column(name = "email")
+    private String email;
+
+    public Student(String firstName, String lastName, int age, LocalDate dob, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public Student(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", dob=" + dob +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
